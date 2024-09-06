8a5b9fd435640f09a98bd705792e40ee91fbe5f1
rce62 <rce62@cornell.edu>
2024-09-06 14:13:34 -0400

    dis 2 implementation

--- PairTripleDetector.v
+++ PairTripleDetector.v
@@ -15,5 +15,14 @@ module PairTripleDetector
   // '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   // Implement a pair/triple detector using explicit gate-level modeling.
 
+  wire w;
+  wire x;
+  wire y;
+
+  or(w, in0, in1);
+  and(x, in0, in1);
+  and(y, w, in2);
+  or(out, y, x);
+
 endmodule
 
